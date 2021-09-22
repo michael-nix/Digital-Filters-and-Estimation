c8d86b45469b85f2e55bd73867fd9fa264b8a93b
michael-nix <61841150+michael-nix@users.noreply.github.com>
2021-09-22 14:33:41 -0400

    Update averaging_emdfilter.m

--- Digital-Filters/averaging_emdfilter.m
+++ Digital-Filters/averaging_emdfilter.m
@@ -13,14 +13,14 @@ for i = 0:(nsegments - 4)
     idx = (nsamples*i + 1):(nsamples*(i + 1));    
     filtered(idx, 1) = emdfilter(f(idx), t(idx), fc);
     
-%     idx = idx + round(nsamples / 4);
-%     filtered(idx, 2) = emdfilter(f(idx), t(idx), fc);
-%     
-%     idx = idx + round(nsamples / 2);
-%     filtered(idx, 3) = emdfilter(f(idx), t(idx), fc);
-%     
-%     idx = idx + round(nsamples * 3 / 4);
-%     filtered(idx, 4) = emdfilter(f(idx), t(idx), fc);
+    idx = idx + round(nsamples / 4);
+    filtered(idx, 2) = emdfilter(f(idx), t(idx), fc);
+    
+    idx = idx + round(nsamples / 2);
+    filtered(idx, 3) = emdfilter(f(idx), t(idx), fc);
+     
+    idx = idx + round(nsamples * 3 / 4);
+    filtered(idx, 4) = emdfilter(f(idx), t(idx), fc);
 end
 
-filtered = sum(filtered, 2); % / 4;
+filtered = sum(filtered, 2) / 4;
